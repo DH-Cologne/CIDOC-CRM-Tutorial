df91608ea2e651aada04dadaf42c0b7e90e85bde
Kai Niebes <kai.niebes@outlook.com>
2020-09-22 13:40:30 +0200

    Chapter tree and resizable video/slides-containers

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,19 @@ if (videoElement && container) {
     location.hash = `#slide-${chapter.no}`;
   });
 }
+
+const chapterTree = document.getElementById('chapter-tree') as HTMLDivElement;
+for (const chapter of cidocCrmChapters) {
+  const div = document.createElement('div');
+  div.classList.add('tree-entry');
+  div.innerText = chapter.title;
+  chapterTree.appendChild(div);
+  div.addEventListener('click', () => {
+    videoElement.currentTime = chapter.timeInSeconds;
+    slideImage.src = `./slides/${chapter.no}.jpg`;
+    location.hash = `#slide-${chapter.no}`;
+  })
+}
+
+slideImage.src = `./slides/1.jpg`;
+location.hash = `#slide-1`;
